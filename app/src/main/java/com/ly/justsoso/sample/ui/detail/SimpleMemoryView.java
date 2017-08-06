c295ebeb33ffb1f960f5ad9b9fb943ccda85fcb3
Liu Yan <ly20050516@gmail.com>
2017-08-06 12:29:59 +0800

    打印更详细的内存状态

--- app/src/main/java/com/ly/justsoso/sample/ui/detail/SimpleMemoryView.java
+++ app/src/main/java/com/ly/justsoso/sample/ui/detail/SimpleMemoryView.java
@@ -2,7 +2,7 @@ package com.ly.justsoso.sample.ui.detail;
 
 import android.app.ActivityManager;
 import android.content.Context;
-import android.os.Binder;
+import android.os.Build;
 import android.os.Debug;
 import android.os.Environment;
 import android.text.format.Formatter;
@@ -13,7 +13,6 @@ import android.widget.TextView;
 import com.ly.justsoso.R;
 
 import java.io.File;
-import java.io.FileDescriptor;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.HashMap;
@@ -40,9 +39,14 @@ public class SimpleMemoryView extends AbstractDetailView {
     @Override
     protected void init(Context context) {
 
+        Debug.startMethodTracing("JustSoSo");
         mTextView = (TextView) findViewById(R.id.simple_memory_text_view_pss);
         StringBuilder stringBuilder = new StringBuilder();
 
+        /**
+         * Debug direct api
+         */
+        stringBuilder.append("Debug direct api" + "\n");
         String sizeStr = Formatter.formatFileSize(getContext(), Debug.getPss() * 1024);
         stringBuilder.append("Pss : " + sizeStr + "\n");
 
@@ -71,78 +75,172 @@ public class SimpleMemoryView extends AbstractDetailView {
         stringBuilder.append("GlobalExternalFreedSize : " + sizeStr + "\n");
 
         if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.M) {
+            stringBuilder.append("\nDebug run time stats\n");
             HashMap<String,String> runtimeStat = (HashMap<String, String>) Debug.getRuntimeStats();
             for(Map.Entry<String,String> entry : runtimeStat.entrySet()) {
                 Log.d(TAG, "init runtime stat : key = " + entry.getKey() + ";value = " + entry.getValue());
+                stringBuilder.append(entry.getKey() + " : " + entry.getValue() + "\n");
             }
         }
 
-        try {
-            File dir = new File(Environment.getExternalStorageDirectory().getAbsolutePath() + File.separator + "hprof");
-            if(!dir.exists()) {
-                dir.mkdir();
+
+        Debug.MemoryInfo debugMemoryInfo = new Debug.MemoryInfo();
+        Debug.getMemoryInfo(debugMemoryInfo);
+
+        /**
+         * total
+         */
+        stringBuilder.append("\nDebug Memory info\n");
+        sizeStr = Formatter.formatFileSize(getContext(), debugMemoryInfo.getTotalPss() * 1024);
+        stringBuilder.append("debugMemoryInfo.getTotalPss() = " + sizeStr + "\n");
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+
+            sizeStr = Formatter.formatFileSize(getContext(), debugMemoryInfo.getTotalPrivateClean() * 1024);
+            stringBuilder.append("debugMemoryInfo.getTotalPrivateClean = " + sizeStr + "\n");
+
+            sizeStr = Formatter.formatFileSize(getContext(), debugMemoryInfo.getTotalPrivateDirty() * 1024);
+            stringBuilder.append("debugMemoryInfo.getTotalPrivateDirty = " + sizeStr + "\n");
+
+            sizeStr = Formatter.formatFileSize(getContext(), debugMemoryInfo.getTotalSharedClean() * 1024);
+            stringBuilder.append("debugMemoryInfo.getTotalSharedClean = " + sizeStr + "\n");
+
+            sizeStr = Formatter.formatFileSize(getContext(), debugMemoryInfo.getTotalSharedDirty() * 1024);
+            stringBuilder.append("debugMemoryInfo.getTotalSharedDirty = " + sizeStr + "\n");
+
+            sizeStr = Formatter.formatFileSize(getContext(), debugMemoryInfo.getTotalSwappablePss() * 1024);
+            stringBuilder.append("debugMemoryInfo.getTotalSwappablePss = " + sizeStr + "\n");
+
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                stringBuilder.append("\nDebug memory stats stats\n");
+                HashMap<String, String> runtimeStat = (HashMap<String, String>) debugMemoryInfo.getMemoryStats();
+                for (Map.Entry<String, String> entry : runtimeStat.entrySet()) {
+                    Log.d(TAG, "init memory stat : key = " + entry.getKey() + ";value = " + entry.getValue());
+                    stringBuilder.append(entry.getKey() + " : " + entry.getValue() + "\n");
+                }
             }
-            File file = new File(dir,"dump.hprof");
-            if(file.exists()){
-                file.delete();
-            }
-            file.createNewFile();
-            long time1 = System.currentTimeMillis();
-//            Debug.dumpHprofData(file.getAbsolutePath());
-            Log.d(TAG, "init: dump hprofdata time = " + (System.currentTimeMillis() - time1));
-        } catch (IOException e) {
-            e.printStackTrace();
         }
 
+        /**
+         * dalvik
+         */
+        sizeStr = Formatter.formatFileSize(getContext(), debugMemoryInfo.dalvikPss * 1024);
+        stringBuilder.append("debugMemoryInfo.dalvikPss = " + sizeStr + "\n");
+
+        sizeStr = Formatter.formatFileSize(getContext(), debugMemoryInfo.dalvikPrivateDirty * 1024);
+        stringBuilder.append("debugMemoryInfo.dalvikPrivateDirty = " + sizeStr + "\n");
+
+        sizeStr = Formatter.formatFileSize(getContext(), debugMemoryInfo.dalvikSharedDirty * 1024);
+        stringBuilder.append("debugMemoryInfo.dalvikSharedDirty = " + sizeStr + "\n");
+
+        /**
+         * native
+         */
+        sizeStr = Formatter.formatFileSize(getContext(), debugMemoryInfo.nativePss * 1024);
+        stringBuilder.append("debugMemoryInfo.nativePss = " + sizeStr + "\n");
+
+        sizeStr = Formatter.formatFileSize(getContext(), debugMemoryInfo.nativePrivateDirty * 1024);
+        stringBuilder.append("debugMemoryInfo.nativePrivateDirty = " + sizeStr + "\n");
+
+        sizeStr = Formatter.formatFileSize(getContext(), debugMemoryInfo.nativeSharedDirty * 1024);
+        stringBuilder.append("debugMemoryInfo.nativeSharedDirty = " + sizeStr + "\n");
+
+        /**
+         * other
+         */
+        sizeStr = Formatter.formatFileSize(getContext(), debugMemoryInfo.otherPss * 1024);
+        stringBuilder.append("debugMemoryInfo.otherPss = " + sizeStr + "\n");
+
+        sizeStr = Formatter.formatFileSize(getContext(), debugMemoryInfo.otherPrivateDirty * 1024);
+        stringBuilder.append("debugMemoryInfo.otherPrivateDirty = " + sizeStr + "\n");
+
+        sizeStr = Formatter.formatFileSize(getContext(), debugMemoryInfo.otherSharedDirty * 1024);
+        stringBuilder.append("debugMemoryInfo.otherSharedDirty = " + sizeStr + "\n");
 
 
-        Debug.dumpService(Context.BATTERY_SERVICE,null,null);
+        /**
+         * Object counts
+         */
+        stringBuilder.append("\nObject Counts\n");
         stringBuilder.append("LoadedClassCount : " + Debug.getLoadedClassCount() + "\n");
 
 
-        Debug.MemoryInfo memoryInfo = new Debug.MemoryInfo();
-        Debug.getMemoryInfo(memoryInfo);
-
-        sizeStr = Formatter.formatFileSize(getContext(),memoryInfo.getTotalPss() * 1024);
-        stringBuilder.append("memoryInfo.getTotalPss : " + sizeStr + "\n");
-
-        ActivityManager.MemoryInfo memoryInfo1 = new ActivityManager.MemoryInfo();
+        /**
+         * AMS memory info
+         */
+        ActivityManager.MemoryInfo memoryInfo = new ActivityManager.MemoryInfo();
         ActivityManager am = (ActivityManager) getContext().getSystemService(Context.ACTIVITY_SERVICE);
-        am.getMemoryInfo(memoryInfo1);
+        am.getMemoryInfo(memoryInfo);
 
-        sizeStr = Formatter.formatFileSize(getContext(),memoryInfo1.totalMem);
+        stringBuilder.append("\nAMS memory info\n");
+
+        sizeStr = Formatter.formatFileSize(getContext(),memoryInfo.totalMem);
         stringBuilder.append("memoryInfo.totalMem : " + sizeStr + "\n");
+        sizeStr = Formatter.formatFileSize(getContext(),memoryInfo.availMem);
+        stringBuilder.append("memoryInfo.availMem : " + sizeStr + "\n");
+        sizeStr = Formatter.formatFileSize(getContext(),memoryInfo.threshold);
+        stringBuilder.append("memoryInfo.threshold : " + sizeStr + "\n");
+        stringBuilder.append("memoryInfo.lowMemory : " + memoryInfo.lowMemory + "\n");
 
         mTextView.setText(stringBuilder.toString());
 
+
+        Debug.stopMethodTracing();
+
+
+    }
+
+    private void dumpHProfData() {
         new Thread(new Runnable() {
             @Override
             public void run() {
-                dumpService(Context.BATTERY_SERVICE);
+                try {
+                    File dir = new File(Environment.getExternalStorageDirectory().getAbsolutePath() + File.separator + "hprof");
+                    if(!dir.exists()) {
+                        dir.mkdir();
+                    }
+                    File file = new File(dir,"dump.hprof");
+                    if(file.exists()){
+                        file.delete();
+                    }
+                    file.createNewFile();
+                    long time1 = System.currentTimeMillis();
+                    Debug.dumpHprofData(file.getAbsolutePath());
+                    Log.d(TAG, "init: dump hprofdata time = " + (System.currentTimeMillis() - time1));
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
             }
         }).start();
-    }
 
-    private void dumpService(String name) {
-        try {
-            File dir = new File(Environment.getExternalStorageDirectory().getAbsolutePath() + File.separator + "service");
-            if(!dir.exists()) {
-                dir.mkdir();
+
+    }
+    private void dumpService(final String name) {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    File dir = new File(Environment.getExternalStorageDirectory().getAbsolutePath() + File.separator + "service");
+                    if(!dir.exists()) {
+                        dir.mkdir();
+                    }
+                    File file = new File(dir,name);
+                    if(file.exists()){
+                        file.delete();
+                    }
+                    file.createNewFile();
+                    FileOutputStream fos = new FileOutputStream(file);
+                    long time1 = System.currentTimeMillis();
+                    Debug.dumpService(name,fos.getFD(),null);
+                    fos.getFD().sync();
+                    fos.close();
+                    Log.d(TAG, "dumpService: name = " + name + ";cost = " + (System.currentTimeMillis() - time1));
+                }catch (Exception e) {
+                    e.printStackTrace();
+                }
             }
-            File file = new File(dir,name);
-            if(file.exists()){
-                file.delete();
-            }
-            file.createNewFile();
-            FileOutputStream fos = new FileOutputStream(file);
-            long time1 = System.currentTimeMillis();
-            Debug.dumpService(name,fos.getFD(),null);
-            fos.getFD().sync();
-            fos.close();
-            Log.d(TAG, "dumpService: name = " + name + ";cost = " + (System.currentTimeMillis() - time1));
-        }catch (Exception e) {
-            e.printStackTrace();
-        }
+        }).start();
+
     }
 
 }
